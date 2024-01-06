1bd5d198defc67b7f05e537647546c109eb02323
Razvan Bidireac <razvan.bidireac@gmail.com>
2024-01-06 20:44:30 +0200

    Uploading the project

---
+++ scripts/Player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+signal took_damage
+
+@onready var rocket_shot_sound = $RocketShotSound
+
+var speed = 300
+var rocket_scene = preload("res://scenes/rocket.tscn")
+@onready var rocket_container = get_node("RocketContainer")
+
+func _physics_process(delta):
+	velocity = Vector2(0,0)
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	move_and_slide()
+	
+	var screen_size = get_viewport_rect().size
+	
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+	
+func shoot():
+	rocket_shot_sound.play()
+	var rocket_instance = rocket_scene.instantiate()
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
+	rocket_instance.global_position.x += 80
+
+func take_damage():
+	emit_signal("took_damage")
+
+func die():
+	queue_free()
+
+
